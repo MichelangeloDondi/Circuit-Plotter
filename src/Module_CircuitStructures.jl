69a02f368ee6f5ba6339c129f5e9cf5391e9f088
MichelangeloDondi <michelangelo.dondi@studio.unibo.it>
2023-10-20 19:31:42 +0200

    v 2.4 Better module CircuitStructures documention

--- src/Module_CircuitStructures.jl
+++ src/Module_CircuitStructures.jl
@@ -22,57 +22,76 @@ module CircuitStructures
     # ============================ Exported Data Structures ========================
     # ==============================================================================
 
-    # For housing the data structures used by the Circuit Visualization Tool
-    export Node, EdgeInfo, Component, Circuit
+        # For housing the data structures used by the Circuit Plotter Program
+        export Node, EdgeInfo, Component, Circuit
 
     # ==============================================================================
     # ============================ Required Packages ===============================
     # ==============================================================================
 
-    # For graph representation of the circuit
-    using LightGraphs
-    
-    """
-    Node
+        # For graph data structures
+        using LightGraphs
+        
+        """
+            struct: Node
 
-    A structure that encapsulates the details of a node.
-    """
-    struct Node
-    id::Int
-    x::Int
-    y::Int
-    end
+        A structure that encapsulates the details of a node.
 
-    """
-    EdgeInfo
+        Fields:
+        - `id`: The unique identifier of the node.
+        - `x`: The x-coordinate of the node.
+        - `y`: The y-coordinate of the node.
+        """
+        struct Node
+            id::Int
+            x::Int
+            y::Int
+        end
 
-    A structure that chronicles the connectivity between diverse nodes.
-    """
-    mutable struct EdgeInfo
-    edges::Vector{Tuple{Int, Int}}
-    end
+        """
+            mutable struct: EdgeInfo
 
-    """
-    Component
+        A structure that chronicles the connectivity between diverse nodes. 
 
-    A structure that encapsulates the details of a component.
-    """
-    struct Component
-    id::Int
-    start_node::Int
-    end_node::Int
-    details::String
-    end
+        Fields:
+        - `edges`: A vector of tuples, where each tuple contains the unique identifiers of the nodes that are connected by an edge.
+        """
+        mutable struct EdgeInfo
+            edges::Vector{Tuple{Int, Int}}
+        end
 
-    """
-    Circuit
+        """
+            struct: Component
 
-    A structure that encapsulates the nodes, components, and their pictorial
-    representation within the circuit.
-    """
-    mutable struct Circuit
-    nodes::Vector{Node}
-    components::Vector{Component}
-    graph::SimpleGraph
-    end
+        A structure that encapsulates the details of a component.
+                
+        Fields:
+        - `id`: The unique identifier of the component.
+        - `start_node`: The unique identifier of the node from which the component originates.
+        - `end_node`: The unique identifier of the node at which the component terminates.
+        - `details`: A string that describes the component.
+        """
+        struct Component
+            id::Int
+            start_node::Int
+            end_node::Int
+            details::String
+        end
+
+        """
+            mutable struct: Circuit
+
+        A structure that encapsulates the nodes, components, and their pictorial
+        representation within the circuit.
+                
+        Fields:
+        - `nodes`: A vector of nodes.
+        - `components`: A vector of components.
+        - `graph`: A graph that represents the connectivity between the nodes.
+        """
+        mutable struct Circuit
+            nodes::Vector{Node}
+            components::Vector{Component}
+            graph::SimpleGraph
+        end
 end
