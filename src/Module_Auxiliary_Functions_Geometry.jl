678e46263b991e5a47d61e57aee50ea2e4a2eab6
MichelangeloDondi <michelangelo.dondi@studio.unibo.it>
2023-10-20 23:22:29 +0200

    v 2.5 Enhanced order and style consistency

--- src/Module_Auxiliary_Functions_Geometry.jl
+++ src/Module_Auxiliary_Functions_Geometry.jl
@@ -8,12 +8,12 @@
     Module Auxiliary_Functions_Geometry
 
 Author: Michelangelo Dondi
-Date: 19-10-2023
+Date: 20-10-2023
 Description:
     Dedicated to housing the geometry functions used by the Circuit Visualization Tool.
     This module simplifies the geometry process by providing a single file to call.
 
-Version: 2.2
+Version: 2.5
 License: MIT License
 
 Exported functions:
@@ -26,69 +26,11 @@ module Auxiliary_Functions_Geometry
     # ==============================================================================
         
         # Invoke this function to check whether a new edge is overlapping for a finite length with an existing edge.
-        export overlapping_edges # Edge overlap detection
+        export overlapping_edges 
 
     # ==============================================================================
-    # ========================== Geometry Functions ================================
+    # ======================= function overlapping_edges ===========================
     # ==============================================================================
-
-        """
-            orientation(p, q, r) -> Int
-
-        Determines the orientation of triplet (p, q, r).
-        Returns:
-        - 0: if collinear
-        - 1: if clockwise
-        - 2: if counterclockwise
-        """
-        function orientation(p, q, r)
-            val = (q[2] - p[2]) * (r[1] - q[1]) - (q[1] - p[1]) * (r[2] - q[2])
-            return val == 0 ? 0 : (val > 0 ? 1 : 2)
-        end
-
-        """
-            strictlyInternalToSegment(A, B, C)  -> Bool
-
-        Checks if point B is strictly inside the segment AC.
-
-        Returns:
-        - true: if point B is strictly internal to the segment AC
-        - false: otherwise
-        """
-            # Returns true if point B is strictly internal to the segment AC.
-            function strictlyInternalToSegment(A, B, C)
-                # Check for a horizontal segment
-                if A[2] == C[2] && A[2] == B[2]
-                    return min(A[1], C[1]) < B[1] < max(A[1], C[1])
-                end
-            
-                # Check for a vertical segment
-                if A[1] == C[1] && A[1] == B[1]
-                    return min(A[2], C[2]) < B[2] < max(A[2], C[2])
-                end
-            
-                # General case
-                x_within = min(A[1], C[1]) < B[1] < max(A[1], C[1])
-                y_within = min(A[2], C[2]) < B[2] < max(A[2], C[2])
-                return x_within && y_within
-            end
-
-        """
-            doOverlap(p1, q1, p2, q2) -> Bool
-
-        Determines if segments (p1, q1) and (p2, q2) overlap.
-
-        Returns:
-        - true: if segments (p1, q1) and (p2, q2) overlap
-        - false: otherwise
-        """
-        function doOverlap(p1, q1, p2, q2)
-            if orientation(p1, q1, p2) != 0 || orientation(p1, q1, q2) != 0
-                return false
-            end
-            return any(strictlyInternalToSegment(p1, p, q1) for p in [p2, q2]) ||
-                any(strictlyInternalToSegment(p2, p, q2) for p in [p1, q1])
-        end
 
         """
             overlapping_edges(new_edge, existing_edges, nodes) -> Vector{Tuple{Int, Tuple{Int, Int}}}
@@ -106,6 +48,76 @@ module Auxiliary_Functions_Geometry
         """
         function overlapping_edges(new_edge, existing_edges, nodes)
             A, B = (nodes[new_edge[1]].x, nodes[new_edge[1]].y), (nodes[new_edge[2]].x, nodes[new_edge[2]].y)
-            return [(index, edge) for (index, edge) in enumerate(existing_edges) if doOverlap(A, B, (nodes[edge[1]].x, nodes[edge[1]].y), (nodes[edge[2]].x, nodes[edge[2]].y))]
+            return [(index, edge) for (index, edge) in enumerate(existing_edges) if _do_overlap(A, B, (nodes[edge[1]].x, nodes[edge[1]].y), (nodes[edge[2]].x, nodes[edge[2]].y))]
         end
+
+    # ==============================================================================
+    # ----------------------------- function _do_overlap -----------------------------
+    # ==============================================================================
+            
+        """
+        _do_overlap(p1, q1, p2, q2) -> Bool
+
+        Determines if segments (p1, q1) and (p2, q2) overlap.
+
+        Returns:
+        - true: if segments (p1, q1) and (p2, q2) overlap
+        - false: otherwise
+        """
+        function _do_overlap(p1, q1, p2, q2)
+            if _orientation(p1, q1, p2) != 0 || _orientation(p1, q1, q2) != 0
+                return false
+            end
+            return any(_strictly_internal_to_segment(p1, p, q1) for p in [p2, q2]) ||
+                any(_strictly_internal_to_segment(p2, p, q2) for p in [p1, q1])
+        end
+
+    # ------------------------------------------------------------------------------
+    # ---------------------------- function _orientation ----------------------------
+    # ------------------------------------------------------------------------------
+
+        """
+            _orientation(p, q, r) -> Int
+
+        Determines the orientation of triplet (p, q, r).
+        Returns:
+        - 0: if collinear
+        - 1: if clockwise
+        - 2: if counterclockwise
+        """
+        function _orientation(p, q, r)
+            val = (q[2] - p[2]) * (r[1] - q[1]) - (q[1] - p[1]) * (r[2] - q[2])
+            return val == 0 ? 0 : (val > 0 ? 1 : 2)
+        end
+
+    # ------------------------------------------------------------------------------
+    # ---------------------- function _strictly_internal_to_segment ---------------------
+    # ------------------------------------------------------------------------------
+
+        """
+            _strictly_internal_to_segment(A, B, C)  -> Bool
+
+        Checks if point B is strictly inside the segment AC.
+
+        Returns:
+        - true: if point B is strictly internal to the segment AC
+        - false: otherwise
+        """
+            # Returns true if point B is strictly internal to the segment AC.
+            function _strictly_internal_to_segment(A, B, C)
+                # Check for a horizontal segment
+                if A[2] == C[2] && A[2] == B[2]
+                    return min(A[1], C[1]) < B[1] < max(A[1], C[1])
+                end
+            
+                # Check for a vertical segment
+                if A[1] == C[1] && A[1] == B[1]
+                    return min(A[2], C[2]) < B[2] < max(A[2], C[2])
+                end
+            
+                # General case
+                x_within = min(A[1], C[1]) < B[1] < max(A[1], C[1])
+                y_within = min(A[2], C[2]) < B[2] < max(A[2], C[2])
+                return x_within && y_within
+            end
 end
