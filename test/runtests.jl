77475ffb0b27d140f26e60b011072fd1337f0bf9
MichelangeloDondi <michelangelo.dondi@studio.unibo.it>
2023-10-22 01:15:47 +0200

    v 2.7 removed test of get_positive_integer_input

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,4 @@ using Test
     include("test_Module_Saving.jl")
 end
 
-@testset "Module_Auxiliary_Functions_Input_Validation Tests" begin
-    println("\nRunning tests for Module_Auxiliary_Functions_Input_Validation...")
-    include("test_Module_Auxiliary_Functions_Input_Validation.jl")
-end
-
 println("\nAll tests have concluded.")
