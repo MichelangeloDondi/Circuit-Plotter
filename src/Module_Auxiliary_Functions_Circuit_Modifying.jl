b7e138ba85affc1239d53f95a08e04b387b4e1d2
MichelangeloDondi <michelangelo.dondi@studio.unibo.it>
2023-10-23 12:39:32 +0200

    v 3.0 created module Auxiliary_Functions_Circuit_Modifying

---
+++ src/Module_Auxiliary_Functions_Circuit_Modifying.jl
@@ -0,0 +1,241 @@
+# ==============================================================================
+# ==============================================================================
+# ============= Module_Auxiliary_Functions_Circuit_Modifying.jl ================
+# ==============================================================================
+# ==============================================================================
+
+"""
+    Module Auxiliary_Functions_Circuit_Modifying
+
+Author: Michelangelo Dondi
+Date: 23-10-2023
+Description: 
+
+Version: 3.0
+License: MIT License
+
+Exported functions: 
+
+"""
+module Auxiliary_Functions_Circuit_Modifying
+
+    # ==============================================================================
+    # =========================== Exported Functions ===============================
+    # ==============================================================================
+
+        # Invokes the modify_existing_node function to modify an existing node's coordinates in the circuit.
+        export modify_existing_node
+
+        # Invokes the delete_node_from_circuit function to delete an existing node from the circuit.
+        export delete_node_from_circuit
+
+    # ==============================================================================
+    # ========================= Imported Data Structure ============================
+    # ==============================================================================
+
+        # For housing the data structures used by the Circuit Plotter Program
+        import Main: Circuit, Node, EdgeInfo
+
+    # ==============================================================================
+    # ============================ Required Packages ===============================
+    # ==============================================================================
+
+        # For graph data structures
+        using LightGraphs
+
+    # ==============================================================================
+    # ============================ Included Modules ================================
+    # ==============================================================================
+#=
+        # Module_Helping.jl provides helper functions for the main program.
+        include("Module_Helping.jl")
+        using .Helping: show_help # Help and instructions
+
+        # Module_Auxiliary_Functions_Circuit_Recap.jl provides auxiliary functions for recapping the circuit.
+        include("Module_Auxiliary_Functions_Circuit_Recap.jl")
+        using .Auxiliary_Functions_Circuit_Recap: show_circuit_recap # Recap the circuit
+        
+        # Module_Plotting.jl provides functions for drawing the current circuit plot.
+        include("Module_Plotting.jl")
+        using .Plotting: draw_plot # Draw the current circuit plot
+
+        # Module_Saving.jl provides functions for saving the plot displayed.
+        include("Module_Saving.jl")
+        using .Saving: save_plot_displayed # Save the plot displayed
+        =#
+
+    # ==============================================================================
+    # ======================== function modify_existing_node =======================
+    # ==============================================================================
+
+        """
+            modify_existing_node(circuit::Circuit) -> nothing
+
+        Modifies an existing node's coordinates in the circuit based on user input.
+
+        # Parameters:
+        - circuit: The primary data structure representing the circuit, including its nodes and components.
+
+        # Returns:
+        - nothing
+        """
+        function modify_existing_node(circuit::Circuit)
+
+            # Continuously prompt the user for node coordinates to modify.
+            while true
+
+                # Print the prompt message.
+                println("\n===================================================")
+                println("\nList of nodes in the circuit:")
+
+                # Print the list of nodes in the circuit.
+                for node in circuit.nodes
+                    println("Node N$(node.id) at position ($(node.x),$(node.y))")
+                end
+
+                # Prompt the user for the node ID.
+                println("\nEnter the ID of the node you want to modify or type 'break' or 'b' to finish modifying nodes:")
+                
+                # Read the node ID from the user.
+                node_id_input = readline()
+
+                # Handle special input (e.g. 'exit', 'help', 'recap', 'draw', 'save', 'break').
+                if node_id_input == "break" || node_id_input == "b"
+
+                    # Provide feedback to the user and break out of the loop.
+                    println("\nFinished modifying nodes.")
+                    break
+                end
+
+                # Try to parse the node ID as an integer.
+                try
+
+                    # Convert node ID to integer.
+                    node_id = parse(Int, node_id_input)
+
+                    # Initialize the found_node to track the node with the given ID.
+                    found_node = nothing
+
+                    # Find the node with the given ID.
+                    for node in circuit.nodes
+
+                        # If the node was found, break out of the loop.
+                        if node.id == node_id   
+
+                            # Update the found_node.
+                            found_node = node
+                            break
+                        end
+                    end
+
+                    # Check if the node was found.
+                    if found_node === nothing
+
+                        # If the node wasn't found, print an error message and continue to the next iteration.
+                        println("\nNode with ID N$node_id not found.")
+                        continue
+                    end
+
+                    # Prompt user for new coordinates.
+                    println("\nProvide the new coordinates for Node N$node_id in the format x,y (coordinates must be integers):")
+                    input_coords = readline()
+
+                    # Split the input and parse coordinates.
+                    coords = split(input_coords, ",")
+                    x, y = parse(Int, coords[1]), parse(Int, coords[2])
+
+                    # Update node coordinates.
+                    found_node.x = x
+                    found_node.y = y
+
+                    # Provide feedback to the user.
+                    println("\nNode N$node_id modified to position ($x,$y).")
+                catch e
+
+                    # If the node ID couldn't be parsed as an integer, print an error message and continue to the next iteration.
+                    println("\nInvalid input: $e")
+                    continue
+                end
+            end
+        end
+
+    # ==============================================================================
+    # ====================== function delete_node_from_circuit =====================
+    # ==============================================================================
+
+        """
+            delete_node_from_circuit(circuit::Circuit) -> nothing
+
+        Deletes an existing node from the circuit based on user input.
+
+        # Parameters:
+        - circuit: The primary data structure representing the circuit, including its nodes and components.
+
+        # Returns:
+        - nothing
+        """
+        function delete_node_from_circuit(node_count::Int, circuit::Circuit)
+
+            # Continuously prompt the user for node coordinates to delete.
+            while true
+
+                # Print the prompt message.
+                println("\n===================================================")
+                println("\nList of nodes in the circuit:")
+
+                for node in circuit.nodes
+                    println("Node N$(node.id) at position ($(node.x),$(node.y))")
+                end
+
+                println("\nEnter the ID of the node you want to delete or type 'break' or 'b' to finish deleting nodes:")
+
+                # Read the node ID from the user.
+                node_id_input = readline()
+
+                if node_id_input in ["break", "b"]
+                    println("\nFinished deleting nodes.")
+                    break
+                end
+
+                # Try to parse the node ID as an integer.
+                try
+
+                    # Convert node ID to integer.
+                    node_id = parse(Int, node_id_input)
+
+                    # Find the node with the given ID.
+                    found_node_idx = findfirst(node -> node.id == node_id, circuit.nodes)
+
+                    # Check if the node was found.
+                    if found_node_idx === nothing   
+
+                        # If the node wasn't found, print an error message and continue to the next iteration.
+                        println("\nNode with ID N$node_id not found.")
+                        continue
+                    end
+        
+                    # Delete the node from the circuit and provide feedback to the user.
+                    deleteat!(circuit.nodes, found_node_idx)
+                    println("\nNode N$node_id deleted.")
+        
+                    # Update the node_count.
+                    node_count -= 1
+
+                    # Update the IDs of nodes after the deleted node.
+                    for i in found_node_idx:length(circuit.nodes)
+                        circuit.nodes[i].id -= 1
+                    end
+
+                    # Rebuild the entire graph from the updated circuit.nodes array.
+                    circuit.graph = SimpleGraph(length(circuit.nodes))
+
+                catch e
+                    println("\nInvalid input: $e")
+                    continue
+                end
+            end
+
+            # Return the updated node_count.
+            return node_count
+        end
+end
--- src/Module_Gathering_Nodes.jl
+++ src/Module_Gathering_Nodes.jl
@@ -17,10 +17,10 @@ Version: 3.0
 License: MIT License
 
 Exported functions:
-- `gather_nodes(circuit::Circuit)`: Systematically assembles information about the nodes
-        present within the circuit, utilizing direct inputs from the user. The accumulated 
-        data finds its place within the `circuit` structure. Additionally, a recap of node 
-        particulars is presented, followed by the graphical portrayal of the updated circuit.
+- `collect_nodes_from_cmd(circuit::Circuit, edgeinfo::EdgeInfo)`: Collects node coordinates 
+from the user and adds them to the provided circuit. The user is prompted to input node
+coordinates or type 'break' or 'b' to end the node collection. The user can also modify
+or cancel existing nodes.
 """
 module Gathering_Nodes
 
@@ -52,6 +52,11 @@ module Gathering_Nodes
         # Module_Auxiliary_Functions_Handle_Special_Input.jl provides auxiliary functions for input handling.
         include("Module_Auxiliary_Functions_Handle_Special_Input.jl")
         using .Auxiliary_Functions_Handle_Special_Input: handle_special_input_break # Handle special input such as 'help', 'recap', 'draw', 'exit', 'break'
+
+        # Module_Auxiliary_Functions_Circuit_Modifying.jl provides auxiliary functions for modifying the circuit.
+        include("Module_Auxiliary_Functions_Circuit_Modifying.jl")
+        using .Auxiliary_Functions_Circuit_Modifying: modify_existing_node # Modify existing nodes
+        using .Auxiliary_Functions_Circuit_Modifying: delete_node_from_circuit # Delete existing nodes
 
     # ==============================================================================
     # ====================== function _collect_nodes_from_cmd ======================
@@ -120,10 +125,18 @@ module Gathering_Nodes
 
                 println("Do you want to modify or to cancel an existing node (default: no)? Type 'm' or 'c' to modify or cancel an existing node.")
                 modify_node = readline()
+
+                # If the user wants to modify some existing nodes, modify them.
                 if modify_node == "m"
+
+                    # Modify the existing nodes.
                     modify_existing_node(circuit)
+
+                # If the user wants to cancel some existing nodes, delete them.
                 elseif modify_node == "c"
-                    delete_node_from_circuit(circuit)
+
+                    # If some nodes are deleted, update the node count.
+                    node_count = delete_node_from_circuit(node_count, circuit)
                 end
             end
         end
@@ -163,6 +176,8 @@ module Gathering_Nodes
 
                 # Check if a node already exists at the provided coordinates.
                 for node in circuit.nodes
+
+                    # If a node already exists at the provided coordinates, print an error message and return false.
                     if node.x == x && node.y == y
                         println("\nNode N",node.id," already exists at position ($x,$y).")
                         return false
@@ -172,6 +187,8 @@ module Gathering_Nodes
                 # Add the node to the circuit if it doesn't already exist.
                 push!(circuit.nodes, Main.Node(idx, x, y))
                 add_vertex!(circuit.graph)
+
+                # Provide feedback to the user and return true.
                 println("\nNode N$idx added at position ($x,$y).")
                 return true
             catch
@@ -181,148 +198,4 @@ module Gathering_Nodes
                 return false
             end
         end
-
-    # ==============================================================================
-    # ------------------------- function modify_existing_node -------------------------
-    # ==============================================================================
-
-        """
-            modify_existing_node(circuit::Circuit) -> nothing
-
-        Modifies an existing node's coordinates in the circuit based on user input.
-
-        # Parameters:
-        - circuit: The primary data structure representing the circuit, including its nodes and components.
-
-        # Returns:
-        - nothing
-        """
-        function modify_existing_node(circuit::Circuit)
-
-            while true
-
-                # Print the prompt message.
-                println("\n===================================================")
-                println("\nList of nodes in the circuit:")
-                
-                for node in circuit.nodes
-                    println("Node N$(node.id) at position ($(node.x),$(node.y))")
-                end
-
-                # Prompt the user for the node ID.
-                println("\nEnter the ID of the node you'd like to modify or type 'break' or 'b' to finish modifying nodes:")
-                
-                # Read the node ID from the user.
-                node_id_input = readline()
-
-                # Handle special input (e.g. 'exit', 'help', 'recap', 'draw', 'save', 'break').
-                if node_id_input == "break" || node_id_input == "b"
-                    println("\nFinished modifying nodes.")
-                    break
-                end
-
-                # Convert node ID to integer.
-                try
-                    node_id = parse(Int, node_id_input)
-
-                    # Find the node with the given ID.
-                    found_node = nothing
-                    for node in circuit.nodes
-                        if node.id == node_id
-                            found_node = node
-                            break
-                        end
-                    end
-
-                    if found_node == nothing
-                        println("\nNode with ID N$node_id not found.")
-                        continue
-                    end
-
-                    # Prompt user for new coordinates.
-                    println("\nProvide the new coordinates for Node N$node_id in the format x,y (coordinates must be integers):")
-                    input_coords = readline()
-
-                    # Split the input and parse coordinates.
-                    coords = split(input_coords, ",")
-                    x, y = parse(Int, coords[1]), parse(Int, coords[2])
-
-                    # Update node coordinates.
-                    found_node.x = x
-                    found_node.y = y
-                    println("\nNode N$node_id modified to position ($x,$y).")
-
-                catch e
-                    println("\nInvalid input: $e")
-                    continue
-                end
-            end
-        end
-
-    # ==============================================================================
-    # ---------------------- function delete_node_from_circuit ---------------------
-    # ==============================================================================
-
-        """
-            delete_node_from_circuit(circuit::Circuit) -> nothing
-
-        Deletes an existing node from the circuit based on user input.
-
-        # Parameters:
-        - circuit: The primary data structure representing the circuit, including its nodes and components.
-
-        # Returns:
-        - nothing
-        """
-        function delete_node_from_circuit(circuit::Circuit)
-
-            while true
-                # Print the prompt message.
-                println("\n===================================================")
-                println("\nList of nodes in the circuit:")
-
-                for node in circuit.nodes
-                    println("Node N$(node.id) at position ($(node.x),$(node.y))")
-                end
-
-                println("\nEnter the ID of the node you'd like to delete or type 'break' or 'b' to finish deleting nodes:")
-
-                # Read the node ID from the user.
-                node_id_input = readline()
-
-                if node_id_input in ["break", "b"]
-                    println("\nFinished deleting nodes.")
-                    break
-                end
-
-                # Convert node ID to integer.
-                try
-                    node_id = parse(Int, node_id_input)
-
-                    # Find the node with the given ID.
-                    found_node_idx = findfirst(node -> node.id == node_id, circuit.nodes)
-
-                    if found_node_idx === nothing
-                        println("\nNode with ID N$node_id not found.")
-                        continue
-                    end
-
-                    # Delete the node from the circuit and its graph representation.
-                    deleteat!(circuit.nodes, found_node_idx)
-                    rem_vertex!(circuit.graph, found_node_idx)
-                    println("\nNode N$node_id deleted.")
-
-                    # Update the IDs of nodes after the deleted node.
-                    for i in found_node_idx:length(circuit.nodes)
-                        circuit.nodes[i].id -= 1
-                    end
-
-                catch e
-                    println("\nInvalid input: $e")
-                    continue
-                end
-            end
-        end
-
-
     end
