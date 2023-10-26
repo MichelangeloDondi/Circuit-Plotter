d01bb12faade4af9fcac691f01c792cc525fa825
MichelangeloDondi <michelangelo.dondi@studio.unibo.it>
2023-10-26 21:36:59 +0200

    v 3.4 Modularized checking_input_is valid

---
+++ src/Module_Auxiliary_Functions_Checking_Input_Of_Nodes.jl
@@ -0,0 +1,96 @@
+# ============================================================================
+# ============================================================================
+# ========== Module_Auxiliary_Functions_Checking_Input_Of_Nodes.jl ===========
+# ============================================================================
+# ============================================================================
+
+"""
+    Module: Auxiliary_Functions_Checking_Input_Of_Nodes  
+
+Author: Michelangelo Dondi
+Date: 26-10-2023
+Description: This module contains the function for checking if the input aimed to add a new node is valid.
+
+Version: 3.4
+License: MIT License 
+
+Exported functions:
+- 'check_if_inupt_is_valid(input::String, circuit::Circuit)::Bool': Checks if the input is valid. 
+The input is valid if it is in the format 'integer x,y' (e.g. '1,-2') and if no node already exists
+at the provided coordinates.
+
+Notes:
+- This module is called by 'Module_Gathering_Nodes.jl'
+- This module is called by 'Module_Auxiliary_Functions_Modifying.jl'
+"""
+
+module Auxiliary_Functions_Checking_Input_Of_Nodes
+
+    # ==============================================================================
+    # =========================== Exported Function ================================
+    # ==============================================================================
+        
+        # Invoke the function to check if the input is valid.
+        export check_if_inupt_is_valid
+    
+    # ==============================================================================
+    # ====================== function check_if_inupt_is_valid ======================
+    # ==============================================================================
+
+        """
+            check_if_inupt_is_valid(input::String, circuit)::Bool  
+
+        Checks if the input is valid. The input is valid if it is in the format 'integer x,y' (e.g. '1,-2') 
+        and if no node already exists at the provided coordinates.
+
+        # Parameters:
+        - input: The input provided by the user.
+        - circuit: The primary data structure representing the circuit, including its nodes and components.
+            
+        # Returns:
+        - true if the input is valid.
+        - false otherwise.
+            
+        # Notes:
+        - This function is called by `process_input`.
+        - This function is called before the node is added to the circuit by `_add_node_to_circuit`.
+            
+        # Examples:
+        ```julia-repl   
+        julia> _check_if_inupt_is_valid("1,-2", circuit)
+        true
+        """
+        function check_if_inupt_is_valid(input::String, circuit)::Bool
+
+            # Split the input into its x and y coordinates.
+            coords = split(input, ",")
+
+            # Try to parse the coordinates as integers.
+            try 
+
+                # Parse the coordinates as integers.
+                x, y = parse(Int, coords[1]), parse(Int, coords[2])
+
+                # Check if a node already exists at the provided coordinates.
+                for node in circuit.nodes
+
+                    # If a node already exists at the provided coordinates, print an error message and return false.
+                    if node.x == x && node.y == y   
+
+                        # Provide feedback to the user and return false.
+                        println("\nThe node cannot be added for the following reason:")
+                        println("Node N",node.id," already exists at position ($x,$y).")
+                        return false
+                    end
+                end
+
+                # If no node already exists at the provided coordinates, return true.
+                return true
+            catch
+
+                # If the coordinates couldn't be parsed as integers, print an error message and return false.
+                println("\nInvalid input. Enter integer coordinates as 'x,y' (e.g. '1,-2').")
+                return false
+            end
+        end
+    end
--- src/Module_Gathering_Nodes.jl
+++ src/Module_Gathering_Nodes.jl
@@ -59,6 +59,10 @@ module Gathering_Nodes
         include("Module_Auxiliary_Functions_Handle_Special_Input.jl")
         using .Auxiliary_Functions_Handle_Special_Input: handle_special_input_break_modify_cancel # Handle the following special input: 'exit', 'help', 'recap', 'draw', 'save', 'break', 'modify', 'cancel'
 
+        # Module_Auxiliary_Functions_Checking_Input_Of_Nodes provides auxiliary functions for checking the input of nodes.
+        include("Module_Auxiliary_Functions_Checking_Input_Of_Nodes.jl")
+        using .Auxiliary_Functions_Checking_Input_Of_Nodes: check_if_inupt_is_valid # Check if the node can be added to the circuit.
+
         # Module_Auxiliary_Functions_Circuit_Modifying.jl provides auxiliary functions for modifying the circuit.
         include("Module_Auxiliary_Functions_Circuit_Modifying.jl")
         using .Auxiliary_Functions_Circuit_Modifying: modify_existing_node # Modify existing nodes
@@ -228,67 +232,6 @@ module Gathering_Nodes
             
             # If the input was invalid, continue to the next iteration.
             return node_count, :continue
-        end
-
-    # ==============================================================================
-    # --------------------- function _check_if_inupt_is_valid ----------------------
-    # ==============================================================================
-
-        """
-            _check_if_inupt_is_valid(input::String, circuit)::Bool  
-
-        Checks if the input is valid. The input is valid if it is in the format 'integer x,y' (e.g. '1,-2') 
-        and if no node already exists at the provided coordinates.
-
-        # Parameters:
-        - input: The input provided by the user.
-        - circuit: The primary data structure representing the circuit, including its nodes and components.
-            
-        # Returns:
-        - true if the input is valid.
-        - false otherwise.
-            
-        # Notes:
-        - This function is called by `process_input`.
-        - This function is called before the node is added to the circuit by `_add_node_to_circuit`.
-            
-        # Examples:
-        ```julia-repl   
-        julia> _check_if_inupt_is_valid("1,-2", circuit)
-        true
-        """
-        function _check_if_inupt_is_valid(input::String, circuit)::Bool
-
-            # Split the input into its x and y coordinates.
-            coords = split(input, ",")
-
-            # Try to parse the coordinates as integers.
-            try 
-
-                # Parse the coordinates as integers.
-                x, y = parse(Int, coords[1]), parse(Int, coords[2])
-
-                # Check if a node already exists at the provided coordinates.
-                for node in circuit.nodes
-
-                    # If a node already exists at the provided coordinates, print an error message and return false.
-                    if node.x == x && node.y == y   
-
-                        # Provide feedback to the user and return false.
-                        println("\nThe node cannot be added for the following reason:")
-                        println("Node N",node.id," already exists at position ($x,$y).")
-                        return false
-                    end
-                end
-
-                # If no node already exists at the provided coordinates, return true.
-                return true
-            catch
-
-                # If the coordinates couldn't be parsed as integers, print an error message and return false.
-                println("\nInvalid input. Enter integer coordinates as 'x,y' (e.g. '1,-2').")
-                return false
-            end
         end
 
     # ------------------------------------------------------------------------------
