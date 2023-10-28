692418c35cfe99aec9084215c0e38e0a6ad2bdc3
MichelangeloDondi <michelangelo.dondi@studio.unibo.it>
2023-10-28 12:58:59 +0200

    v 4.1 Added color-coding system

--- src/main.jl
+++ src/main.jl
@@ -11,19 +11,25 @@
     Circuit Plotter Program
 
 Author: Michelangelo Dondi
-Date: 27-10-2023
+Date: 28-10-2023
 Description:
     A user-friendly tool that allows the creation and visualization of electrical circuits.
     Users can define nodes, components, and their connections, with an end visualization
     rendered using the PlotlyJS backend for interactivity.
 
-Version: 3.5
+Version: 4.1
 License: MIT License
 
 Notes:
 - The program is structured in modules, each of which is described in the corresponding file.
 - The main module is Module_Main_Function.jl, which contains the main function of the program.
 - The program is written in Julia, a high-level, high-performance, dynamic programming language for numerical computing.
+- The program uses this color-coding system:
+    - \033[31m (Red):     Errors and failures (e.g. wrong input, etc.) 
+    - \033[32m (Green):   Success and confirmation (e.g. correct input, etc.)
+    - \033[33m (Yellow):  Useful information (e.g. number of nodes already defined, etc.)
+    - \033[36m (Cyan):    Instructions and comunications (e.g. "Press enter to continue", etc.)
+    - \033[0m  (White):   Other
 """
 
 # ==============================================================================
