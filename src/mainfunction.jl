c2939a91d4ec77a3bffa4bd6c6b4ddbbd92b369c
MichelangeloDondi <michelangelo.dondi@studio.unibo.it>
2023-10-29 00:48:58 +0200

    v 4.4 Put a loop to let the user to start again. Minor improvements present as well

--- src/mainfunction.jl
+++ src/mainfunction.jl
@@ -15,7 +15,7 @@ Description:
     This module orchestrates the execution of the various modules that comprise the program,
     and provides the user with feedback and instructions as necessary.
 
-Version: 4.3
+Version: 4.4
 License: MIT License
         
 Exported functions:
@@ -24,21 +24,21 @@ Exported functions:
 module MainFunction
 
     # ==============================================================================
-    # ============================ Exported Function ===============================
+    # ============================== Exported Function =============================
     # ==============================================================================
 
         # main function of the program 
         export main
     
     # ==============================================================================
-    # =========================== Exported Variables ===============================
+    # ============================= Exported Variables =============================
     # ==============================================================================
 
         # Use these variables to access the data structures used by the Circuit Plotter Program
         export circuit, edge_info
 
     # ==============================================================================
-    # ============================ Required Packages ===============================
+    # ============================== Required Packages =============================
     # ==============================================================================
 
         # For graph data structures
@@ -86,15 +86,25 @@ module MainFunction
         edge_info = EdgeInfo([])
 
     # ==============================================================================
-    # =============================== Main Function ================================
+    # ============================== Function: main() ==============================
     # ==============================================================================
 
         """
-            main() -> nothing   
+            main()
 
         The main function of the program. It orchestrates the execution of the
         various modules that comprise the program, and provides the user with
-        feedback and instructions as necessary.
+        feedback and instructions as necessary. The program starts by greeting
+        the user and providing any necessary instructions or information. Then,
+        the program enters a loop that allows the user to create as many circuits
+        as desired. For each circuit, the program gathers the particulars of the
+        nodes, then gathers the particulars of the edges and of the components,
+        and provides feedback to the user. After the circuit is created, the
+        program recaps the circuit, draws the circuit plot, and asks the user
+        whether to create another circuit. If the user does not want to create
+        another circuit, the program exits. Otherwise, the program starts over.
+        Before exiting the program, the program asks the user whether to save the
+        plot displayed.
             
         Parameters:
         - none
@@ -107,17 +117,32 @@ module MainFunction
             # Greet the user and provide any necessary instructions or information.
             show_initial_greetings()
 
-            # Gather the particulars of the nodes.
-            collect_nodes(circuit, edge_info)
+            while true
+                
+                # Gather the particulars of the nodes.
+                collect_nodes(circuit, edge_info)
 
-            # Gather the particulars of the edges and of the components and provide feedback to the user.
-            collect_edges(circuit, edge_info)
+                # Gather the particulars of the edges and of the components and provide feedback to the user.
+                collect_edges(circuit, edge_info)
 
-            # Recap the circuit.
-            show_circuit_recap(circuit, edge_info)
+                # Recap the circuit.
+                show_circuit_recap(circuit, edge_info)
 
-            # Draw the circuit plot.
-            draw_plot(circuit)
+                # Draw the circuit plot.
+                draw_plot(circuit)
+
+                println("""
+                
+                Do you want to create another circuit?\033[36m
+                println("Type 'y' for yes or 'n' for no.
+                println("If you type 'n', the program will exit.
+                println("If you type 'y', or anything else, the program will start over.""")
+
+                # Check whether the user wants to create another circuit.
+                if readline(stdin) == "n"
+                    break
+                end
+            end
 
             # Ask the user whether to save the plot displayed before exiting the program.
             show_final_greetings_asking_whether_to_save_plot_displayed(circuit)
